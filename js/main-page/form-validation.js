7fea7c283acd80ad78b47a630813eeb2dd40bb0d
Mayzer <Tolik.Korzunov@yandex.ru>
2025-04-29 18:13:22 +0300

    Added catalog navigation in header

--- js/main-page/form-validation.js
+++ js/main-page/form-validation.js
@@ -145,12 +145,14 @@ function updateTransform() {
 
 
 
+// Поиск в хедере
 
 const searchInput = document.getElementById('search-input');
 const header = document.querySelector('.header-top');
 const searchItems = document.querySelector('.search-items');
 const underHeaderContainer = document.querySelector('.under-header-container');
 const underHeader = document.querySelector('.under-header');
+let isCatalogActive = false;
 
 // Обработчик ввода в поле поиска
 searchInput.addEventListener('input', function () {
@@ -163,7 +165,7 @@ searchInput.addEventListener('input', function () {
         underHeaderContainer.style.marginTop = '0px';
         underHeader.style.filter = 'blur(5px)';
         underHeader.style.backdropFilter = 'blur(5px)';
-    } else {
+    } else if (!isCatalogActive) {
         // Сбрасываем фон header, если поиск пуст
         header.style.backgroundColor = 'transparent';
 
@@ -172,5 +174,152 @@ searchInput.addEventListener('input', function () {
         underHeaderContainer.style.marginTop = '250px';
         underHeader.style.filter = 'none';
         underHeader.style.backdropFilter = 'none';
+    } else {
+        // Скрываем элементы с плавным переходом
+        searchItems.classList.remove('show');
+        underHeaderContainer.style.marginTop = '0px';
+        underHeader.style.filter = 'blur(5px)';
+        underHeader.style.backdropFilter = 'blur(5px)';
     }
+});
+
+
+// Нажатие на кнопку каталог в хедере
+
+const menuNavigation = document.querySelector('.menu-navigation');
+const openCatalog = document.querySelector('.open-catalog');
+
+openCatalog.addEventListener('click', function (e) {    
+    
+    e.preventDefault(); // чтобы не перезагружалась страница
+    this.parentElement.classList.toggle('active');
+
+    underHeaderContainer.style.marginTop = '0px';
+    underHeader.style.filter = 'blur(5px)';
+    underHeader.style.backdropFilter = 'blur(5px)';
+
+    // Проверяем, активирован ли класс и выводим результат в консоль
+    const isActive = this.parentElement.classList.contains('active');
+    isCatalogActive = isActive;
+
+    if (isCatalogActive) {
+        // Когда каталог активирован, меняем фон и показываем меню
+        openCatalog.style.color = '#D4B28C';
+        header.style.backgroundColor = '#151c28';
+        menuNavigation.style.backgroundColor = '#151c28';
+        menuNavigation.style.display = 'block';
+    } else {
+        // Когда каталог деактивирован, скрываем меню и сбрасываем стиль
+        openCatalog.style.color = '';
+        header.style.backgroundColor = 'transparent';
+        menuNavigation.style.display = 'none';
+
+        underHeaderContainer.style.marginTop = '250px';
+        underHeader.style.filter = 'none';
+        underHeader.style.backdropFilter = 'none';
+
+        searchItems.classList.remove('show');
+
+        // Сбросить поле поиска
+        searchInput.value = '';
+    }
+});
+
+
+
+// Отображение меню Сантехники
+document.querySelectorAll('.menu-toggle-plumbing').forEach(toggle => {
+    toggle.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const menuItem = toggle.closest('.menu-item-plumbing');
+        const subMenu = menuItem.querySelector('.submenu-plumbing');
+
+        const isOpen = subMenu.style.display === 'flex';
+        subMenu.style.display = isOpen ? 'none' : 'flex';
+        menuItem.classList.toggle('active', !isOpen);
+
+        underHeaderContainer.style.marginTop = isOpen ? '0px' : '0px';
+    });
+});
+
+
+document.querySelectorAll('.menu-toggle-furniture').forEach(toggle => {
+    toggle.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const menuItem = toggle.closest('.menu-item-furniture');
+        const subMenu = menuItem.querySelector('.submenu-furniture');
+
+        const isOpen = subMenu.style.display === 'flex';
+        subMenu.style.display = isOpen ? 'none' : 'flex';
+        menuItem.classList.toggle('active', !isOpen);
+    });
+});
+
+
+// Делегирование для правого меню
+const rightMenuContent = document.querySelector('.right-menu-content');
+
+// Назначаем обработчики на все .shower-enclosures-and-pallets
+document.querySelectorAll('.shower-enclosures-and-pallets').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        rightMenuContent.innerHTML = `
+            <a href="#">Душевые уголки, двери и перегородки</a>
+            <a href="#">Поддоны</a>
+            <a href="#">Шторки для ванны</a>
+            <div class="all-categories-link">
+                <a href="#">Все товары категории</a>
+                            
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 8L16.6854 13.2929C17.1049 13.6834 17.1049 14.3166 16.6854 14.7071L11 20" stroke="#EDE4D7" stroke-linecap="round"/>
+                </svg>  
+            </div>
+        `;
+        rightMenuContent.style.display = 'flex';
+    });
+});
+
+// Назначаем обработчики на все .plumbing
+document.querySelectorAll('.plumbing').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        rightMenuContent.innerHTML = `
+            <a href="#">Новые душевые кабины</a>
+            <a href="#">Новые поддоны</a>
+            <a href="#">Новые шторки</a>
+            <div class="all-categories-link">
+                <a href="#">Все товары категории</a>
+                            
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 8L16.6854 13.2929C17.1049 13.6834 17.1049 14.3166 16.6854 14.7071L11 20" stroke="#EDE4D7" stroke-linecap="round"/>
+                </svg>  
+            </div>
+        `;
+        rightMenuContent.style.display = 'flex';
+    });
+});
+
+
+// Назначаем обработчики на все .plumbing
+document.querySelectorAll('.shower-program').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        rightMenuContent.innerHTML = `
+            <a href="#">Лейки и держатели</a>
+            <a href="#">Душевые стойки и штанги</a>
+            <a href="#">Гибкие шланги</a>
+            <a href="#">Верхний душ</a>
+            <a href="#">Готовые душевые комплекты</a>
+            <div class="all-categories-link">
+                <a href="#">Все товары категории</a>
+                            
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 8L16.6854 13.2929C17.1049 13.6834 17.1049 14.3166 16.6854 14.7071L11 20" stroke="#EDE4D7" stroke-linecap="round"/>
+                </svg>  
+            </div>
+        `;
+        rightMenuContent.style.display = 'flex';
+    });
 });
